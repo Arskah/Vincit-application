ba172b68d225429bb47a1b203e73dbac7e482236
Aarni Halinen <aarni.halinen@aalto.fi>
2019-02-06 22:42:22 +0200

    Integrate Redux counter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import counter from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counter);
+const rootEl = document.getElementById('root');
+const render = () => ReactDOM.render(
+  <App store={store} />,
+  rootEl);
+render()
+store.subscribe(render);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
